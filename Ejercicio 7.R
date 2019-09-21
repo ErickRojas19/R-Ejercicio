a2bb653df3096644ecbd5c42615e128755ca65fa
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-09-20 22:46:17 -0500

    Rename Ejercicio 5.R to Ejercicio 7.R

---
+++ Ejercicio 7.R
@@ -0,0 +1,11 @@
+a = as.integer(readline(prompt="Digite un número: "))
+b = as.integer(readline(prompt="Digite otro número: "))
+suma = a+b
+resta = a-b
+mult = a*b
+div = a/b
+resi = a%%b
+paste(suma,"Suma")
+paste(resta,"Resta")
+paste(mult,"Multiplicación")
+paste(div,resi,"División y residuo")
