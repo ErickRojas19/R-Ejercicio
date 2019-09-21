bd5891a4aa250d9af24430ce4a77b7a8d08c3f2f
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-09-21 00:48:02 -0500

    Delete Ejercicio 11.R

--- Ejercicio 11.R
+++
@@ -1,5 +0,0 @@
-a = as.integer(readline(prompt="Digite un numero: "))
-b = as.integer(readline(prompt="Digite un numero: "))
-c = as.integer(readline(prompt="Digite un numero: "))
-pro = (a+b+c)/3
-print(pro)
