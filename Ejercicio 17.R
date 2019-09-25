e19f4021dda33c98382e44c853e35e1ac9e5965f
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-09-25 11:30:10 -0500

    Add files via upload

---
+++ Ejercicio 17.R
@@ -0,0 +1,5 @@
+masa = as.numeric(readline(prompt="Digite el peso en kg: "))
+luz = 299792458
+
+ener =(masa*(luz*luz)/2)
+print(ener)
---
+++ Ejercicio 18.R
@@ -0,0 +1,4 @@
+m = as.numeric(readline(prompt="Digite la masa: "))
+a = as.numeric(readline(prompt="Digite la aceleración: "))
+f = m*a
+paste("La fuerza es",f)
