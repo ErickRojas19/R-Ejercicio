5fcb11325766558c06065487fee184835fd6111f
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-09-27 12:31:40 -0500

    Add files via upload

---
+++ Ejercicio 16.R
@@ -0,0 +1,6 @@
+a = as.numeric(readline(prompt="Dijite la aceleración: "))
+t = as.numeric(readline(prompt="Dijite el tiempo: "))
+
+vf = a*t**2/2
+
+paste("La velocidad Final es",vf)
---
+++ Ejercicio 19.R
@@ -0,0 +1,7 @@
+x1 = as.integer(readline(prompt="Digite la cordenada: "))
+x2 = as.integer(readline(prompt="Digite la cordenada: "))
+y1 = as.integer(readline(prompt="Digite la cordenada: "))
+y2 = as.integer(readline(prompt="Digite la cordenada: "))
+
+d=sqrt(((x2-x1)**2)+((y2-y1)**2))
+paste("La distancia de las cordenadas es",d)
