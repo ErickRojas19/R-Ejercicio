eb4e545e66fde8c5e40baa24e2aa3fa5b0e8d9a0
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-10-22 11:17:23 -0500

    Add files via upload

---
+++ ejercicio 27.R
@@ -0,0 +1,6 @@
+n = as.integer(readline(prompt="Digite un número: "))
+if(n %% 2 == 0){
+print("Es par")
+} else {
+print("Es in-par")
+}
---
+++ ejercicio 28.R
@@ -0,0 +1,6 @@
+n = as.integre(readline(prompt="Digite un número: "))
+if(n>0){
+print("Positivo")
+} else {
+print("Negativo")
+}
