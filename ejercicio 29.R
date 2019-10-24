678b89f4e4c86e5005fdd3368dbbf169f6e1190f
ErickRojas19 <47455082+ErickRojas19@users.noreply.github.com>
2019-10-24 08:54:43 -0500

    Update ejercicio 29.R

--- ejercicio 29.R
+++ ejercicio 29.R
@@ -1,10 +1,10 @@
-n = as.integre(readline(prompt="Digite un número: "))
-if(n>0 &&  n %% 2 == 0){
+n = as.integre(readline(prompt="Digite un nÃºmero: "))
+if(n>0 &&  n %% 2 <- 0){
 print("El numero es par positivo")
 } else{  
 print("El numero es in-par Positivo")
 }
-if(n<0 && n %%2 == 0){
+if(n<0 && n %%2 <- 0){
 print("Es Negativo par")
 } else{
 print("Es negativo in-par")
